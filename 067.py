abe5733c530cae0cba4875d5d6c47bf987c723dc
Maxwell Plotkin <plotkinmaxwell@gmail.com>
2018-04-13 12:46:32 -0700

    solutions for 018 and 067

---
+++ 067.py
@@ -0,0 +1,30 @@
+# Find the maximum total from top to bottom of the triangle
+dag = []
+
+
+def main():
+    file = open("p067_triangle.txt")
+
+    for line in file:
+        dag.append(line.split(' '))
+
+    for layer in range(len(dag)):
+        for node in range(len(dag[layer])):
+            dag[layer][node] = int(dag[layer][node]) + max_parent(layer, node)
+
+    print(max(dag[-1]))
+
+
+def max_parent(l, n):
+    if l is 0:
+        return 0
+    elif n is 0:
+        return dag[l-1][0]
+    elif n is l:
+        return dag[l-1][n-1]
+    else:
+        return max(dag[l-1][n-1], dag[l-1][n])
+
+
+if __name__ == '__main__':
+    main()
